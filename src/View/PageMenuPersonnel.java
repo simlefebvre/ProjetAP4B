01b6e6b76c89647ce1ca8b71fb5e793e8ee5a403
aymeP <58934999+aymeP@users.noreply.github.com>
2021-06-04 19:07:05 +0200

    Update PageMenuPersonnel.java

--- src/View/PageMenuPersonnel.java
+++ src/View/PageMenuPersonnel.java
@@ -1,7 +1,10 @@
 package View;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -9,14 +12,29 @@ import javax.swing.JPanel;
  */
 
 public class PageMenuPersonnel extends Fenetre{
-	private JPanel pan;
+	//private JPanel pan;
+	//private JButton bouton;
 	
 	public PageMenuPersonnel() {
-		pan = new JPanel();
-		//Définition de sa couleur de fond
-	    pan.setBackground(Color.ORANGE); 
+		//JPanel pan = new JPanel();
+	    Box b1 = Box.createHorizontalBox();
+	    JButton bouton = new JButton("Afficher le matériel disponible");
+	    /*pan*/b1.add(bouton);
+	    
+	    Box b2 = Box.createHorizontalBox();
+	    JButton bouton2 = new JButton("Afficher le matériel réservé");
+	    /*pan*/b2.add(bouton2);
+	    
+	    
+	    Box b3 = Box.createVerticalBox();
+	    b3.add(b1);
+	    b3.add(b2);
+	    
 	    
 	    //On prévient notre JFrame que notre JPanel sera son content pane
-	    fenetre.setContentPane(pan);
+	    fenetre.getContentPane(/*pan*/).add(b3,BorderLayout.EAST);
+	    //fenetre.getContentPane();
+	    
+	    //TODO A faire finir par Simon!    
 	}
 }
