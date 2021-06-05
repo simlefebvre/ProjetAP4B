cebc5950dd59d7ebf3d0357e0f62b3d0b92acc22
Simon Lefebvre <simon.lefebvre@utbm.fr>
2021-06-05 18:56:28 +0200

    changement taille fenetre

--- src/View/Fenetre.java
+++ src/View/Fenetre.java
@@ -20,7 +20,7 @@ public class Fenetre extends JFrame{
 	    //Positionner au centre
 	    fenetre.setLocationRelativeTo(null);
 	    //Définir taille minimale
-	    fenetre.setMinimumSize(new Dimension(100, 100));
+	    fenetre.setMinimumSize(new Dimension(500, 500));
 	    
 	    //Termine le processus lorsqu'on clique sur la croix rouge
 	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
