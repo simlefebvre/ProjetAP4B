d01f4599f6cd68830d57c5fe0e815f72dc385be5
Simon Lefebvre <simon.lefebvre@utbm.fr>
2021-06-05 17:34:23 +0200

    Controler Connexion

--- src/Controller/MainClass.java
+++ src/Controller/MainClass.java
@@ -1,13 +1,18 @@
 package Controller;
 
+import Model.DataBase.BaseDeDonee;
+import Model.Utilisateur.Utilisateur;
 //import View.PageMenuPersonnel;
 import View.PageConnexion;
+import View.ViewManager;
 
 public class MainClass {
 	
 	public static void main(String[] args) {
-		PageConnexion con = new PageConnexion();
+		//PageConnexion con = new PageConnexion();
 		//PageMenuPersonnel menuPerso = new PageMenuPersonnel();
+		ViewManager vm = new ViewManager();
+		BaseDeDonee.getInstance().addUtilisateur(new Utilisateur("admin", "admin", "admin", "admin"));
 	}
 
 }
