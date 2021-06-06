bbf82b33edb9d60667070cb658287e6dc9a76099
Simon Lefebvre <simon.lefebvre@utbm.fr>
2021-06-06 20:30:35 +0200

    fin jours 3

--- src/Controller/listeMaterielController.java
+++ src/Controller/listeMaterielController.java
@@ -4,6 +4,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import View.ViewManager;
 
@@ -17,12 +18,24 @@ public class listeMaterielController implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		System.out.println("test");
 		if(e.getSource() instanceof JButton) {
 			JButton but = (JButton) e.getSource();
-			
-			if(but.getName().equalsIgnoreCase("retour")) {
+			String name = but.getName();
+			if(name.equalsIgnoreCase("retour")) {
 				this.vm.pAffMat.close();
 				this.vm.showMenuProf();
+			}else if(name.contains("suppr")){
+				char num[] = {'','',''};
+				name.getChars(4, name.length(), num, 0);
+				
+				int index = Integer.parseInt(new String(num));
+				
+				int choix = JOptionPane.showOptionDialog(null, "Etes vous sur de vouloir supprimer le materiel n°"+ index + " ?" , "Supression" , JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE , null , null, null);
+				
+				if(choix == JOptionPane.YES_OPTION) {
+					//TODO continuer ici
+				}
 			}
 		}
 	}
